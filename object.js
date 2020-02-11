7a090f332480fedeb64ea111f84609c39985ac36
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-11 13:42:16 -0700

    finished object exercies

---
+++ object.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// Object Creator
+
+
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }
+  };
+}
+
+// console.log(createMyObject());
+
+
+// Object Updater
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+let test1 = createMyObject();
+
+// console.log(updateObject(test1));
+
+
+// Self Reference
+
+function personMaker() {
+  let person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+  return person;
+}
+let person = personMaker();
+// console.log(person.fullName());
+
+
+// Deleting Keys
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
+
+
